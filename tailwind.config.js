6bf6a9c8a84cd1f8e99cfa493110771ec7fed389
developedbyrayhan <islamraihanofficial@gmail.com>
2023-11-06 11:36:23 +0600

    live site setup

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    'node_modules/preline/dist/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: ["emerald"],
+  },
+  plugins: [
+    require("daisyui"),
+    require('preline/plugin'),
+  ],
+}
+
