c6139e95cc0c1df183142d2e2c6ca0c8895e1699
developedbyrayhan <islamraihanofficial@gmail.com>
2023-11-09 21:36:36 +0600

    updated

--- src/pages/JoinCard.jsx
+++ src/pages/JoinCard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const JoinCard = ({user}) => {
     return (
         <div key={user._id} className='flex flex-col items-center justify-center gap-2'>
-            <img className='w-36 lg:w-48 rounded-full' src={user?.volunteerImage} />
+            <img className='w-36 h-36 object-cover lg:w-48 lg:h-48 shadow rounded-full' src={user?.volunteerImage} />
             <h1 className='text-xl lg:text-2xl text-[#2e355a] font-medium'>{user?.volunteerName}</h1>
             <p className='text-[#50577A] font-extralight'>{user?.volunteerEmail}</p>
         </div>
